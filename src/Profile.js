74ebaa7c9e060f2487106e6ab7d6c4c5e88a83b9
Kishore-1022 <kishoresmart229@gmail.com>
2023-12-02 15:22:50 +0530

    Verify User Email

--- src/Profile.js
+++ src/Profile.js
@@ -25,6 +25,7 @@ const Profile = () => {
             name.current.value=data.users[0].displayName
             url.current.value=data.users[0].photoUrl
           }
+          console.log(data.users)
          }catch(err){
           console.log(err.message)
         };
@@ -32,6 +33,26 @@ const Profile = () => {
     useEffect(()=>{
       getDetails();
     },[]);
+
+    const verifyHandler=async()=>{
+      try{
+        const res=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDW06_RxkmqCUsLd7-gYF9mGl0bDsIaHLs',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            requestType: 'VERIFY_EMAIL',
+            idToken: ctx.token,
+        })
+    });
+    }catch(err){
+      console.log(err.message)
+    }
+    alert('Verification link has been sent to your emailId')
+  }
+
+
   
    
    
@@ -46,9 +67,9 @@ const Profile = () => {
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                  idToken: ctx.token, // Get the user's ID token
-                  displayName: name.current.value, // Updated display name
-                  photoUrl: url.current.value, // Updated photo URL
+                  idToken: ctx.token,
+                  displayName: name.current.value,
+                  photoUrl: url.current.value,
                   returnSecureToken: true,
                 }),
               });  
@@ -70,8 +91,13 @@ const Profile = () => {
             </div>      
         </div> 
     </div> 
+    <div className='button-right'>
+     Please Verify your Email: <Button variant="primary" type="submit" onClick={verifyHandler}>Verify Email</Button>
+    </div>
+    
     <Form onSubmit={sumbitHandler}>
         <div className='d-flex gap-2 mt-4'> 
+
             <Form.Group className="mb-3">
                 <Form.Label> Full Name:</Form.Label>
                 <Form.Control type="text" placeholder="Enter Name" ref={name}/>
